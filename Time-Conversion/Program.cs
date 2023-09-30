d6b667ea5da61b58c112018d259f8e83b23c94c4
Abaidullah Asif <129421093+Abaidullah889@users.noreply.github.com>
2023-10-01 00:00:22 +0200

    Create Program.cs

---
+++ Time-Conversion/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+class Result
+    {
+        public static string timeConversion(string s)
+        {
+
+        string APs = s.Substring(s.Length - 2);
+        s = s.Remove(s.Length - 2);
+        string []timeparts = s.Split(":");
+
+            int hh, mm, ss;
+            hh = Convert.ToInt32(timeparts[0]);
+            mm = Convert.ToInt32(timeparts[1]);
+            ss = Convert.ToInt32(timeparts[2]);
+
+
+            if (APs == "PM" && hh != 12)
+            {
+                hh += 12;
+            }
+            else if (APs == "AM" && hh == 12)
+            {
+                hh = 0;
+            }
+
+            string res = $"{hh}:{mm}:{ss}";
+            return res;
+
+
+        }
+
+
+
+    }
+
+
+     class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        string s= Console.ReadLine();
+        s=Result.timeConversion(s);
+        Console.WriteLine(s);
+
+        }
+    }
