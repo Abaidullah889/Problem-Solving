02998e1c06a6b9f28c1f0c56f37c1b782f975fa5
Abaidullah Asif <129421093+Abaidullah889@users.noreply.github.com>
2023-10-01 14:36:14 +0200

    Test

---
+++ Test1/Mock-Test-1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Solving
+{
+
+    class res
+    {
+
+        public static int Median(List<int> arr)
+        {
+
+            arr.Sort();
+
+            int size = arr.Count;
+            if (size % 2 == 0)
+            {
+
+                int mid1 = arr[size / 2 + 1];
+                int mid2 = arr[size / 2];
+                return (mid1 + mid2) / 2;
+
+            }
+            else
+            {
+
+                return arr[size / 2];
+            }
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+    internal class Mock_Test_1
+    {
+        static void Main()
+        {
+            List<int> arr = new List<int>();
+
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            string s = Console.ReadLine();
+
+            string[] fstring = (s.Split(" "));
+
+            for (int i = 0; i < size; i++)
+            {
+                arr.Add(Convert.ToInt32(fstring[i]));
+            }
+
+            int me = res.Median(arr);
+            Console.WriteLine(me);
+
+
+        }
+    }
+}
+
+
+
+
