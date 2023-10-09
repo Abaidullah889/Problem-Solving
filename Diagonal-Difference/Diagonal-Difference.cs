dc9f26ee275de5620b1b1f686a366143c048594a
Abaidullah Asif <129421093+Abaidullah889@users.noreply.github.com>
2023-10-09 15:42:45 +0200

    Another Problem!

---
+++ Diagonal-Difference/Diagonal-Difference.cs
@@ -0,0 +1,56 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int diagonalDifference(List<List<int>> arr)
+    {
+        int fsum = 0;
+        int ssum = 0;
+        for (int i = 0; i < arr.Count; i++)
+        {
+            fsum += arr[i][i];
+            ssum += arr[i][arr.Count - i - 1];
+        }
+
+        int result = Math.Abs(fsum - ssum);
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        Console.WriteLine(Result.diagonalDifference(arr));
+    }
+}
---
+++ Diagonal-Difference/Readme.txt
@@ -0,0 +1,10 @@
+In this example, you have to find the the absolute difference between the sums of the matrix's two diagonals as a single integer.
+
+Input:
+3
+11 2 4
+4 5 6
+10 8 -12
+
+Output:
+15
